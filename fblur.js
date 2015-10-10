29017d1660d8e80fa60cd06a7a811ddae61cc9fd
Eric Typaldos <ettypaldos@gmail.com>
2015-10-10 16:53:39 -0700

    Reactive render for cubes

--- fblur.js
+++ fblur.js
@@ -1,29 +1,66 @@
 if (Meteor.isClient) {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.25, 1000 );
   renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
 
   // Set camera distance
   camera.position.z = 5;
 
+  cubes = [];
+
   Meteor.startup(function () {
     document.body.appendChild( renderer.domElement );
-    goCube();
+
+    Tracker.autorun(function () {
+      cubes = [];
+      Cubes.find().forEach(function (cubeDoc) {
+        var geo = new THREE.BoxGeometry(cubeDoc.geometry[0], cubeDoc.geometry[1], cubeDoc.geometry[2]);
+        var material = new THREE.MeshBasicMaterial(cubeDoc.meshOptions);
+        var cube = new THREE.Mesh(geo, material);
+        // Add to scene
+        scene.add(cube);
+        // Save to cubes
+        cubes.push(cube);
+      });
+    });
+
     render();
   });
+
+  render = function () {
+      requestAnimationFrame( render );
+      cubes.forEach(function (cube) {
+        cube.rotation.x += Math.random()*0.1;
+        cube.rotation.y += Math.random()*0.1;
+      });
+
+      renderer.render(scene, camera);
+  };
+
+
+  goCube = function () {
+      geometry = new THREE.BoxGeometry( 1, 1, 1 );
+      material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+      cube = new THREE.Mesh( geometry, material );
+      scene.add( cube );
+  };
 }
-render = function () {
-    requestAnimationFrame( render );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    renderer.render(scene, camera);
-};
 
-
-goCube = function () {
-    geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-    cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
-};
+// Add seed data
+Meteor.startup(function () {
+  if (Cubes.find().count() < 3) {
+    Cubes.insert({
+      geometry: [1,1,1],
+      meshOptions: { color: 0xffffff }
+    });
+    Cubes.insert({
+      geometry: [1,1,1],
+      meshOptions: { color: 0xcccccc }
+    });
+    Cubes.insert({
+      geometry: [1,1,1],
+      meshOptions: { color: 0xfc0000 }
+    });
+  }
+});
