f5531d8431e3f73770592875b1745e500f4fec36
Eric Typaldos <ettypaldos@gmail.com>
2015-10-10 15:49:46 -0700

    Cleanup

--- fblur.js
+++ fblur.js
@@ -1,17 +1,17 @@
 if (Meteor.isClient) {
-    Meteor.startup(function () {
-        scene = new THREE.Scene();
-        camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-        renderer = new THREE.WebGLRenderer();
-        renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( renderer.domElement );
-        geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
-        cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
-        camera.position.z = 5;
-        render();
-    });
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+  // Set camera distance
+  camera.position.z = 5;
+
+  Meteor.startup(function () {
+    document.body.appendChild( renderer.domElement );
+    goCube();
+    render();
+  });
 }
 render = function () {
     requestAnimationFrame( render );
@@ -20,8 +20,10 @@ render = function () {
     renderer.render(scene, camera);
 };
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+
+goCube = function () {
+    geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    material = new THREE.MeshBasicMaterial( { color: 0xffffff } );
+    cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+};
