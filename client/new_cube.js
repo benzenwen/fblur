9a02279efd2fda9e6c57cb08262d5a98a02738c0
Micah Alcorn <admin@micahalcorn.com>
2015-10-10 20:59:51 -0500

    Handle unexpected color input

--- client/new_cube.js
+++ client/new_cube.js
@@ -5,12 +5,28 @@ Template.NewCube.events({
     var width = t.$('input[name="cube-geometry-width"]').val().trim() || 1;
     var height = t.$('input[name="cube-geometry-height"]').val().trim() || 1;
     var depth = t.$('input[name="cube-geometry-depth"]').val().trim() || 1;
+    var color = t.$('input[name="cube-material-color"]').val().trim();
     var cubeObj = {
       geometry: [parseInt(width), parseInt(height), parseInt(depth)],
-      meshOptions: {
-        color: t.$('input[name="cube-material-color"]').val().trim() || 0xffffff
-      }
+      meshOptions: {}
     };
+
+    if (color) {
+      if (color.length <= 7) {
+        // ensure proper syntax
+        if (color[0] === '#') {
+          cubeObj.meshOptions.color = color;
+        } else {
+          cubeObj.meshOptions.color = '#' + color;
+        }
+      } else {
+        // unknown input
+        cubeObj.meshOptions.color = color;
+      }
+    } else {
+      // default to example
+      cubeObj.meshOptions.color = 0xff0000;
+    }
 
     Cubes.insert(cubeObj, function (err, res) {
       if (err) {
