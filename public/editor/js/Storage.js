5ed5770de03724ab3035a13425d710b998747d7d
Ben Wen <github@benwen.com>
2015-10-11 12:00:02 -0700

    Shared mesh state separated from camera state.

--- public/editor/js/Storage.js
+++ public/editor/js/Storage.js
@@ -1,56 +1,126 @@
 /**
- * @author benwen / http://benwen.com/
- * Meteor.js version
+ * @author mrdoob / http://mrdoob.com/
  */
 
 var Storage = function () {
 
-  return {
+	var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
-	init: function ( callback ) {
-      // nothing to init with Meteor
-
-      callback();
-
-	},
-
-	get: function ( callback ) {
-
-      var result = Scenes.findOne(); // TODO Fixme for more than one.
-
-      if (result) {
-        console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Got state from minimongo. ' + result.data);
-	    callback( JSON.parse(result.data) );    
-      } else {
-        callback ();
-      }
-	},
-
-	set: function ( data, callback ) {
-
-	  var start = performance.now();
-
-      var result = Scenes.upsert({"_id": "MASTER"}, {"data": JSON.stringify(data)});
-
-      if (result.numberAffected == 1) {
-		console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Saved state to minimongo. ' + ( Performance.now() - start ).toFixed( 2 ) + 'ms' );
-      } else {
-        console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Failes to save to minimongo. ' + ( performance.now() - start ).toFixed( 2 ) + 'ms' );
-	  }
-	},
-
-	clear: function ( callback ) {
-
-      var result = Scenes.remove ({"_id": "MASTER"});
-
-      if (result == 1) {
-        console.log( 'Deleted MASTER' );
-       }
-
-	  callback();
-
+	if ( indexedDB === undefined  ) {
+		console.warn( 'Storage: IndexedDB not available.' );
+		return { init: function () {}, get: function () {}, set: function () {}, clear: function () {} };
 	}
 
-  }
+	var name = 'threejs-editor';
+	var version = 1;
+
+	var database;
+
+	return {
+
+		init: function ( callback ) {
+
+			var request = indexedDB.open( name, version );
+			request.onupgradeneeded = function ( event ) {
+
+				var db = event.target.result;
+
+				if ( db.objectStoreNames.contains( 'states' ) === false ) {
+
+					db.createObjectStore( 'states' );
+
+				}
+
+			};
+			request.onsuccess = function ( event ) {
+
+				database = event.target.result;
+
+				callback();
+
+			};
+			request.onerror = function ( event ) {
+
+				console.error( 'IndexedDB', event );
+
+			};
+
+
+		},
+
+		get: function ( callback ) {
+
+            // Meteor
+            var compositeResult;
+            var sceneResult = Scenes.findOne(); // TODO Fixme for more than one.
+
+            if (sceneResult) {
+                console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Got state from minimongo. ' + sceneResult.data);
+	            compositeResult = JSON.parse(sceneResult.data);    
+            } 
+
+            // IndexDB
+			var transaction = database.transaction( [ 'states' ], 'readwrite' );
+			var objectStore = transaction.objectStore( 'states' );
+			var request = objectStore.get( 0 );
+			request.onsuccess = function ( event ) {
+                compositeResult.camera = event.target.result;
+				callback( compositeResult );
+
+			};
+
+		},
+
+		set: function ( data, callback ) {
+
+			var start = performance.now();
+
+            var cameraData = data.camera;
+            delete data.camera;
+
+            // Meteor
+            var result = Scenes.upsert({"_id": "MASTER"}, {"data": JSON.stringify(data)});
+
+            if (result.numberAffected == 1) {
+		        console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Saved state to minimongo. ' + ( performance.now() - start ).toFixed( 2 ) + 'ms' );
+            } else {
+                console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Failed to save to minimongo. ' + ( performance.now() - start ).toFixed( 2 ) + 'ms' );
+	        }
+            
+            data.camera = cameraData; // Put it back just in case. TODO check this.
+
+            // IndexDB
+			var transaction = database.transaction( [ 'states' ], 'readwrite' );
+			var objectStore = transaction.objectStore( 'states' );
+			var request = objectStore.put( cameraData, 0 );
+			request.onsuccess = function ( event ) {
+
+				console.log( '[' + /\d\d\:\d\d\:\d\d/.exec( new Date() )[ 0 ] + ']', 'Saved camera to IndexedDB. ' + ( performance.now() - start ).toFixed( 2 ) + 'ms' );
+
+			};
+
+		},
+
+		clear: function ( callback ) {
+
+            // Meteor
+            var result = Scenes.remove ({"_id": "MASTER"});
+
+            if (result == 1) {
+                console.log( 'Deleted MASTER' );
+            }
+
+			var transaction = database.transaction( [ 'states' ], 'readwrite' );
+			var objectStore = transaction.objectStore( 'states' );
+			var request = objectStore.clear();
+			request.onsuccess = function ( event ) {
+
+				callback();
+
+			};
+
+		}
+
+	}
 
 };
